c54f6b980b9eac339d03e170c992daeb4772bd0f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-06-10 13:39:08 +0100

    Initial commit - generate project

---
+++ lib/sajad_foodie.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "sajad_foodie/version"
+
+module SajadFoodie
+  class Error < StandardError; end
+
+  # Your code goes here...
+end
---
+++ lib/sajad_foodie/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SajadFoodie
+  VERSION = "0.1.0"
+end
