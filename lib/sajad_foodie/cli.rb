6c61a88044ee3430ab84ac3f6d84d9137a9e9637
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-06-10 14:41:09 +0100

    Create generator

--- lib/sajad_foodie/cli.rb
+++ lib/sajad_foodie/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'sajad_foodie'
+require 'sajad_foodie/generators/recipe'
 
 module SajadFoodie
   class CLI < Thor
@@ -12,6 +13,11 @@ module SajadFoodie
     method_option :word, aliases: '-w'
     def pluralize
       puts SajadFoodie::Food.pluralize(options[:word])
+    end
+
+    desc 'recipe', 'Generates a recipe scaffold'
+    def recipe(group, name)
+      SajadFoodie::Generators::Recipe.start([group, name])
     end
   end
 end
